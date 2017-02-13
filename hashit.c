2951a018dd98d60f6090895c54af8298b324f850
Catherine Javadian <cjavadia@stevens.edu>
2017-02-13 10:12:28 -0500

    Update hashit.c

--- hashit.c
+++ hashit.c
@@ -13,80 +13,75 @@ typedef struct {
     int num_keys;
 } hash_set;
 
-char * EMPTY = NULL;
+char * EMPTY = "empty";
 
 void clear_table(hash_set *set) {
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		if (set->keys[i] != NULL && set->keys[i] != EMPTY) {
-			set->keys[i] = NULL;
+	int count = 0;
+	while (count < TABLE_SIZE) {
+		if (set->keys[count] != NULL) {
+			//free(set->keys[count]);
+			set->keys[count] = NULL;
 		}
+		count++;
 	}
 	return;
 }
 
 int hash(char *key) {
-	int ch = 0;
-	int hash = 0;
+	int sum = 0;
 	int check = 0;
+	int i = 1;
 	while(key[check]) {
-		ch += (key[check])*(check+1);
+		sum += (key[check])*(i);
 		check++;
+		i++;
 	}
-	ch *= 19;
-	hash = (ch % TABLE_SIZE);
-//	printf("%i\n", hash);
-	return hash;
+	sum *= 19;
+	return (sum % TABLE_SIZE);
+}
+
+int in_hash(hash_set *set, char *key) {
+	int hashk = hash(key);
+	int k = 0;
+	int i = 0;
+	while (i < 20) {
+		k = (hashk + (i*i) + (i*23))%TABLE_SIZE;
+		if (set->keys[k] == NULL) {
+			return 0;
+		}
+		if (strcmp(key, set->keys[k]) == 0) {
+			return -1;
+		}
+		i++;
+	}
+	return 0;
 }
 
 int insert_key(hash_set *set, char *key) {
 	int hashk = hash(key);
 	int k = hashk;
-	int count = 1;
-	while (count != 21) {
-		if(set->keys[k] == NULL || set->keys[k] == EMPTY) {
-			set->keys[k] = key;
-			return 0;
+	int count = 0;
+	if (in_hash(set, key) == 0) {
+		while (count <= 20) {
+			if (set->keys[k] == EMPTY || set->keys[k] == NULL) {
+				set->keys[k] = key;
+				return 0;
+			}
+			if (strcmp(key, set->keys[k]) == 0) {
+				return -1;
+			}
+			k = (hashk + (count*count) + (count*23))%TABLE_SIZE;
+			count++;
 		}
-//		if(set->keys[k] == EMPTY) {
-//			printf("%s\n", "here");
-//			int tempc = (21 - count);
-//			int temp = k;
-//			while (tempc != 0) {
-//				if(strcmp(key, set->keys[temp]) == 0) {
-//					return -1;
-//				}
-//				temp = (k + (tempc*tempc) + (tempc*23))%TABLE_SIZE;
-//				tempc--;
-//			}
-//			set->keys[k] = key;
-//		}
-		if (strcmp(key, set->keys[k]) == 0) {
-			return -1;
-		}
-		k = (hashk + (count*count) + (count*23))%TABLE_SIZE;
-		count++;
 	}
 	return -1;
 }
-//	while (set->keys[hashk] != NULL) {
-//		if (strcmp(key, set->keys[hashk]) == 0) {
-//			return -1;
-//		}
-//		hashk += ((count*count) + (count*23))%TABLE_SIZE;
-//		count++;
-//		if (count == 19) {
-//			return -1;
-//		}
-//	}
-//	set->keys[hashk] = key;
-//	return 0;
-//}
 
 int delete_key(hash_set *set, char *key) {
 	int hashk = hash(key);
 	int k = hashk;
-	int count = 1;
-	while (count != 21) {
+	int count = 0;
+	while (count <= 20) {
 		if (set->keys[k] == NULL) {
 			return -1;
 		}
@@ -100,15 +95,17 @@ int delete_key(hash_set *set, char *key) {
 		k = (hashk + (count*count) + (count*23))%TABLE_SIZE;
 		count++;
 	}
-	return 0;
+	return -1;
 }
 
 void display_keys(hash_set *set) {
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		if (set->keys[i] != NULL) {
+	int i = 0;
+	while (i < TABLE_SIZE) {
+		if (set->keys[i] != NULL && set->keys[i] != EMPTY) {
 			printf("%i:",i);
 			printf("%s\n",set->keys[i]);
 		}
+		i++;
 	}
 	return;
 }
@@ -117,16 +114,19 @@ int main() {
 	int lines;
 	int ops;
 	hash_set * hash = (hash_set *)malloc(sizeof(hash_set));
-	for (int i = 0; i > TABLE_SIZE; i++) {
-		hash->keys[i] = NULL;
+	int count = 0;
+	while (count < TABLE_SIZE) {
+		hash->keys[count] = NULL;
+		count++;
 	}
+	char * input = (char *)malloc(sizeof(char)*19);
+	char * key;
 	scanf("%d", &lines);
 	while (lines > 0) {
 		scanf("%d", &ops);
 		while (ops > 0) {
-			char * input = (char *)malloc(sizeof(char)*19);
 			scanf("%s", input);
-			char *key = (char *)malloc(sizeof(char)*15);
+			key = (char *)malloc(sizeof(char)*15);
 			char check = input[0];
 			strncpy(key, input+4, 15);
 			if (check == 'A') {
@@ -143,8 +143,12 @@ int main() {
 		printf("%i\n", hash->num_keys);
 		display_keys(hash);
 		clear_table(hash);
+		free(key);
+		hash->num_keys = 0;
 		lines--;
 	}
+	free(hash);
+	free(input);
 	return 0;
 }
 
